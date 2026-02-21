54cba55545f0b96bc356e4de5e68a44365ae0672
Kseniia <xeniyyaa@MacBook-Air-Kseniia.local>
2026-02-21 19:11:54 +0200

    fix: website id 6 and zone id 8

--- components/NextAdsScript.tsx
+++ components/NextAdsScript.tsx
@@ -3,6 +3,6 @@
 import Script from "next/script";
 
 export default function NextAdsScript() {
-  return <Script src="https://api-dev.next-ads-server-dev.com/script/tag.js?websiteId=6" 
+  return <Script src="https://api-dev.next-ads-server-dev.com/script/tag.js?websiteId=8" 
   strategy="afterInteractive" />;
 }
